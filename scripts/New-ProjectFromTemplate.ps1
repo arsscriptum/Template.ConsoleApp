f73f5d77dffa0d4161e0125d0335becc8576eca0
Guillaume Plante <guillaumeplante.qc@gmail.com>
2022-08-26 02:49:20 -0400

    auto

---
+++ scripts/New-ProjectFromTemplate.ps1
@@ -0,0 +1,187 @@
+<#
+#̷𝓍   𝓐𝓡𝓢 𝓢𝓒𝓡𝓘𝓟𝓣𝓤𝓜 
+#̷𝓍 
+#̷𝓍   <guillaumeplante.qc@gmail.com>
+#̷𝓍   https://arsscriptum.github.io/  
+#>
+
+[CmdletBinding(SupportsShouldProcess)]
+    param(
+        [Parameter(Mandatory=$true, Position=0)]
+        [String]$DestinationPath,
+        [Parameter(Mandatory=$true, Position=1)]
+        [String]$ProjectName,
+        [Parameter(Mandatory=$false)]
+        [String]$BinaryName,
+        [Parameter(Mandatory=$false)]
+        [switch]$Overwrite
+    )  
+
+
+
+try{
+
+
+    # Script Variables
+    $Script:ProjectRoot = (Resolve-Path "$PSScriptRoot\..").Path
+    $Script:TemplatePath = $Script:ProjectRoot
+    $Script:DependenciesPath = (Resolve-Path "$PSScriptRoot\dependencies").Path
+    $Script:Importer         = (Resolve-Path "$PSScriptRoot\cmdlets\Import-Dependencies.ps1").Path
+    $Script:BuildCfgFile = Join-Path $Script:TemplatePath "buildcfg.ini"
+    $Script:ProjectFile = Join-Path $Script:TemplatePath "vs\__PROJECT_NAME__.vcxproj"
+    $Script:FiltersFile = Join-Path $Script:TemplatePath "vs\__PROJECT_NAME__.vcxproj.filters"
+    $Script:ConfigsFile = Join-Path $Script:TemplatePath "vs\cfg\winapp.props"
+    $Script:DejaInsFile = Join-Path $Script:TemplatePath "vs\cfg\dejainsight.props"
+
+    $Script:NewBuildCfgFile = Join-Path $Path "buildcfg.ini"
+    $Script:NewProjectFile = Join-Path $Path "vs\$($ProjectName).vcxproj"
+    $Script:NewFiltersFile = Join-Path $Path "vs\$($ProjectName).vcxproj.filters"
+    $Script:NewConfigsFile = Join-Path $Path "vs\cfg\winapp.props"
+    $Script:NewDejaInsFile = Join-Path $Path "vs\cfg\dejainsight.props"
+
+    $Script:ProjectFiles = @($Script:BuildCfgFile,$Script:ProjectFile, $Script:FiltersFile, $Script:ConfigsFile, $Script:DejaInsFile)
+    $Script:NewProjectFiles = @($Script:NewBuildCfgFile,$Script:NewProjectFile, $Script:NewFiltersFile, $Script:NewConfigsFile, $Script:NewDejaInsFile )
+    . "$Script:Importer" -Path "$Script:DependenciesPath"
+
+
+    Write-Log "Script:TemplatePath $Script:TemplatePath"
+    
+    $Guid = (New-Guid).Guid
+    $Guid = "{$Guid}"
+
+Write-Log "DestinationPath $DestinationPath"
+    # Apply default values if required
+
+    if($False -eq $PSBoundParameters.ContainsKey('BinaryName')){
+        $BinaryName = $ProjectName
+        LogMessage "BinaryName: Using default value `"$ProjectName`""
+    }
+
+    $Script:Verbose = $False
+    if($PSBoundParameters.ContainsKey('Verbose')){
+        $Script:Verbose = $True
+    }elseif($Verbose -eq $Null){
+        $Script:Verbose = $False
+    }
+
+    $ErrorOccured = $False
+    $TestMode = $False
+    if($PSBoundParameters.ContainsKey('WhatIf')){
+        LogMessage "TESTMODE ENABLED"
+        $TestMode = $true
+    }
+
+    if(Test-Path -Path $DestinationPath -PathType Container){
+        if($Overwrite){
+            Remove-Item "$DestinationPath" -Force -Recurse -ErrorAction Ignore | Out-Null  
+        }
+    }else{
+        $Null = New-Item -Path $DestinationPath -ItemType Directory -Force -ErrorAction Ignore
+        $Null = Remove-Item -Path $DestinationPath -ItemType Directory -Recurse -ErrorAction Ignore
+    }
+    $s = Get-Date -uFormat %d
+    $LogFile = "$ENV:Temp\log.$s.log"
+    Invoke-Robocopy -Source $Script:TemplatePath -Destination "$DestinationPath" -SyncType 'MIRROR' -ExcludedPath @('.git', '.vs') -Log $LogFile
+
+
+    $Logs = Get-Content $LogFile
+    ForEach($l in $Logs){
+        Write-Host "$l" -DarkCyan
+    }
+    
+
+    return
+
+    For($x = 0 ; $x -lt $NewProjectFiles.Count ; $x++){
+        $newfile = $NewProjectFiles[$x]
+        if(Test-Path -Path $newfile -PathType Leaf){
+            if($Overwrite){
+                Remove-Item "$NewProjectFiles[$y]" -Force -ErrorAction Ignore | Out-Null ; LogMessage "DELETED `"$NewProjectFiles[$y]`"" -d;
+                continue;
+            }
+            LogMessage "Overwrite `"$newfile`" (y/n/a) " -n
+            $a = Read-Host '?'
+            while(($a -ne 'y') -And ($a -ne 'n') -And ($a -ne 'a')){
+                LogMessage "Please enter `"y`" , `"n`" or `"a`""
+                LogMessage "Overwrite `"$newfile`" (y/n/a) " -n
+                $a = Read-Host '?'
+            }
+            if($a -eq 'a'){  
+                For($y = 0 ; $y -lt $NewProjectFiles.Count ; $y++){
+                    Remove-Item "$NewProjectFiles[$y]" -Force -ErrorAction Ignore | Out-Null ; LogMessage "DELETED `"$NewProjectFiles[$y]`"" -d;
+                }
+                break;
+            }
+            elseif($a -ne 'y'){ throw "File $newfile already exists!" ; }else{ Remove-Item $newfile -Force -ErrorAction Ignore | Out-Null ; LogMessage "DELETED `"$newfile`"" -d;}
+        }
+    }
+    For($x = 0 ; $x -lt $ProjectFiles.Count ; $x++){
+        $file = $ProjectFiles[$x]
+        $newfile = $NewProjectFiles[$x]
+        LogMessage "Processing '$file'" -d
+        $Null = Remove-Item -Path $newfile -Force -ErrorAction Ignore
+        $Null = New-Item -Path $newfile -ItemType File -Force -ErrorAction Ignore
+        $exist = Test-Path -Path $file -PathType Leaf
+        if($Verbose){
+            LogResult "CHECKING FILE `"$file`"" -Ok:$exist
+        }
+
+        
+        if($exist -eq $False){    
+            throw "Missing $file"
+        }
+        
+
+        LogMessage "Get-Content -Path $file -Raw" -d
+        $FileContent = Get-Content -Path $file -Raw
+        if($FileContent -eq $null){ throw "INVALID File $file" }
+
+        LogMessage "`$FileContent.IndexOf('__PROJECT_NAME__')" -d
+        $i = $FileContent.IndexOf('__PROJECT_NAME__')
+        if($i -ge 0){
+            LogMessage "Replacing '__PROJECT_NAME__' to '$ProjectName'" -d
+            $FileContent = $FileContent -Replace '__PROJECT_NAME__', $ProjectName    
+        }
+        $i = $FileContent.IndexOf('__PROJECT_GUID__')
+        if($i -ge 0){
+            LogMessage "Replacing '__PROJECT_GUID__' to '$Guid'" -d
+            $FileContent = $FileContent -Replace '__PROJECT_GUID__', $Guid
+        }
+        $i = $FileContent.IndexOf('__PROJECT_GUID__')
+        if($i -ge 0){
+            LogMessage "Replacing '__BINARY_NAME__' to '$BinaryName'" -d
+            $FileContent = $FileContent -Replace '__BINARY_NAME__', $BinaryName
+        }
+
+
+        
+        LogMessage "Saving '$newfile'"
+        Set-Content -Path $newfile -Value $FileContent
+    }
+    
+}catch{
+    $ErrorOccured = $True
+    ShowExceptionDetails $_ -ShowStack
+}finally{
+    if($ErrorOccured -eq $False){
+        Write-Host "`n[SUCCESS] " -ForegroundColor DarkGreen -n
+        if($TestMode){
+            Write-Host "test success! You can rerun in normal mode" -ForegroundColor Gray
+        }else{
+            Write-Host "Project generated in $Path" -ForegroundColor Gray    
+            $exp = (Get-Command 'explorer.exe').Source
+            &"$exp" "$Path"
+        }
+    }else{
+        Write-Host "`n[FAILED] " -ForegroundColor DarkRed -n
+        Write-Host "Script failure" -ForegroundColor Gray
+    }
+}
+
+
+
+
+
+        
+        
+    
---
+++ src/main.cpp
@@ -0,0 +1,445 @@
+﻿
+//==============================================================================
+//
+//     main.cpp
+//
+//============================================================================
+//  Copyright (C) Guilaume Plante 2020 <cybercastor@icloud.com>
+//==============================================================================
+
+
+
+#include "stdafx.h"
+#include "win32.h"
+#include "cmdline.h"
+#include "Shlwapi.h"
+#include "log.h"
+#include "httplib.h"
+#include "jsmn.h"
+#include <codecvt>
+#include <locale>
+#include <vector>
+#include <unordered_map>
+#include <iterator>
+#include <regex>
+#include <filesystem>
+#include <iostream>
+#include <sstream>
+
+#ifdef USING_HTTPLIB
+#pragma message( "USING HTTPLIB" )
+#endif
+
+using namespace httplib;
+using namespace std;
+
+#pragma message( "Compiling " __FILE__ )
+#pragma message( "Last modified on " __TIMESTAMP__ )
+
+void banner();
+void usage();
+void logError(const char * msg);
+char* parseJson(const char* js, int len);
+vector<string> split(const string& s, char delim);
+static int jsoneq(const char* json, jsmntok_t* tok, const char* s);
+string dump_headers(const Headers& headers);
+string dump_params(const Params& params);
+bool downloadFile(string dwl_url, string fullUrl, string outFile, bool optVerbose);
+char* getDlUrl(string filename, string api_url, bool optVerbose);
+string log(const Request& req, const Response& res);
+void writeFileBytes(const char* filename, const char* buffer, int size);
+bool testConnection(const char * host, int port, time_t conn_timeout_sec = 5, time_t read_timeout_sec = 5, time_t write_timeout_sec = 5);
+
+
+void testLogFunctions(const char * msg);
+
+int main(int argc, TCHAR** argv, TCHAR envp)
+{
+
+#ifdef UNICODE
+	const char** argn = (const char**)C::Convert::allocate_argn(argc, argv);
+#else
+	char** argn = argv;
+#endif // UNICODE
+
+	CmdLineUtil::getInstance()->initializeCmdlineParser(argc, argn);
+
+	CmdlineParser* inputParser = CmdLineUtil::getInstance()->getInputParser();
+
+	CmdlineOption cmdlineOptionHelp({ "-h", "--help" }, "display this help");
+	CmdlineOption cmdlineOptionVerbose({ "-v", "--verbose" }, "verbose output");
+	CmdlineOption cmdlineOptionPath({ "-p", "--path" }, "path");
+	CmdlineOption cmdlineOptionNoBanner({ "-n", "--nobanner" }, "no banner");
+	CmdlineOption cmdlineOptionWhatIf({ "-w", "--whatif" }, "whatif");
+
+	inputParser->addOption(cmdlineOptionHelp);
+	inputParser->addOption(cmdlineOptionVerbose);
+	
+	inputParser->addOption(cmdlineOptionNoBanner); 
+	inputParser->addOption(cmdlineOptionWhatIf); 
+	inputParser->addOption(cmdlineOptionPath); 
+
+
+	bool optHelp = inputParser->isSet(cmdlineOptionHelp);
+	bool optVerbose = inputParser->isSet(cmdlineOptionVerbose);
+	bool optPath= inputParser->isSet(cmdlineOptionPath);
+	bool optNoBanner = inputParser->isSet(cmdlineOptionNoBanner);
+	bool optWhatIf = inputParser->isSet(cmdlineOptionWhatIf);
+	if(optNoBanner == false){
+		banner();
+	}
+	if(optHelp){
+		usage();
+		return 0;
+	}
+	string destinationPath = ".";
+	if (optPath) {
+		destinationPath = inputParser->getCmdOption("-p");
+	}
+
+
+	testLogFunctions(destinationPath.c_str());
+	return 0;
+  
+	auto my_url = "https://arsscriptum.github.io";
+	auto api_url = "https://api.fosshub.com";	
+	auto dwl_url = "https://download.fosshub.com";	
+	
+  	auto filename = "iview460_x64_setup.exe"; 
+    auto release_id = "623457812413750bd71fef36";    
+    auto project_id = "5b8d1f5659eee027c3d7883a";
+	
+	string dest = destinationPath + "\\" + filename;
+
+	bool strApiConnected = testConnection("api.fosshub.com",443);
+	COUTMM("testConnection on %s --> ", "api.fosshub.com");
+	COUTY("%s", strApiConnected?"CONNECTION SUCCESS":"CONNECTION FAILED");
+
+	if (!strApiConnected){
+		logError("Server unreachable. Exiting...");
+		return -1;
+	}
+ 
+	COUTMM("testConnection on %s --> ", "download.fosshub.com");
+	bool strDownloadConnected = testConnection("download.fosshub.com",443);
+	COUTY("%s", strDownloadConnected?"CONNECTION SUCCESS":"CONNECTION FAILED");
+	if (!strDownloadConnected){
+		logError("Server unreachable. Exiting...");
+		return -1;
+	}
+ 
+	// ==================================================================
+	// GET iview460_x64_setup.exe
+	// ==================================================================
+	char *fullUrl = getDlUrl(filename, api_url, optVerbose);
+	if (optWhatIf) {
+		COUTRS("WhatIf: No Download. %s\n", fullUrl);
+		return 0;
+	}
+	if (fullUrl) {
+		bool downloaded = downloadFile(dwl_url, fullUrl, dest, optVerbose);
+		if (!downloaded){
+			logError("Download failed. Exit...");
+			return -1;
+		}
+	}
+
+	// ==================================================================
+	// GET iview460_plugins_x64_setup.exe
+	// ==================================================================
+	filename = "iview460_plugins_x64_setup.exe";
+	dest = destinationPath + "\\" + filename;
+	fullUrl = getDlUrl(filename, api_url, optVerbose);
+	if (optWhatIf) {
+		COUTRS("WhatIf: No Download. %s\n", fullUrl);
+		return 0;
+	}
+	
+	if (fullUrl) {
+		bool downloaded = downloadFile(dwl_url, fullUrl, dest, optVerbose);
+		if (!downloaded){
+			logError("Download failed. Exit...");
+			return -2;
+		}
+	}
+
+	return 0;
+}
+
+char* parseJson(const char *js, int len){
+
+	int i;
+	int r;
+	jsmn_parser p;
+	jsmntok_t t[128]; /* We expect no more than 128 tokens */
+
+	jsmn_init(&p);
+	r = jsmn_parse(&p, js, len, t, sizeof(t) / sizeof(t[0]));
+	if (r < 0) {
+	    COUTRS("Failed to parse JSON: %d\n", r);
+	    return nullptr;
+	}
+
+	std::string url_res = "";
+  	for (i = 1; i < r; i++) {
+    	if (jsoneq(js, &t[i], "url") == 0) {
+    	  
+    	  char *str = (char*)malloc(512);
+	      /* We may use strndup() to fetch string value */
+	      sprintf(str, "%.*s", t[i + 1].end - t[i + 1].start, js + t[i + 1].start);
+	      i++;
+	      
+	      return str;
+	    }
+  	}
+  return nullptr;
+}
+
+
+
+vector<string> split(const string& s, char delim) {
+	vector<string> result;
+	stringstream ss(s);
+	string item;
+
+	while (getline(ss, item, delim)) {
+		result.push_back(item);
+	}
+
+	return result;
+}
+
+
+static int jsoneq(const char* json, jsmntok_t* tok, const char* s) {
+	if (tok->type == JSMN_STRING && (int)strlen(s) == tok->end - tok->start &&
+		strncmp(json + tok->start, s, tok->end - tok->start) == 0) {
+		return 0;
+	}
+	return -1;
+}
+
+
+string dump_headers(const Headers& headers) {
+	string s;
+	char buf[BUFSIZ];
+
+	for (const auto& x : headers) {
+		snprintf(buf, sizeof(buf), "%s: %s\n", x.first.c_str(), x.second.c_str());
+		s += buf;
+	}
+
+	return s;
+}
+
+string dump_params(const Params& params) {
+	string s;
+	char buf[BUFSIZ];
+
+	for (const auto& x : params) {
+		snprintf(buf, sizeof(buf), "%s: %s\n", x.first.c_str(), x.second.c_str());
+		s += buf;
+	}
+
+	return s;
+}
+
+
+void banner() {
+	std::wcout << std::endl;
+	COUTC("getfh v2.1 - TOOL TO GET FOSSHUB FILES\n");
+	COUTC("Built on %s\n", __TIMESTAMP__);
+	COUTC("Copyright (C) 2000-2021 Guillaume Plante\n");
+	std::wcout << std::endl;
+}
+void usage() {
+	COUTCS("Usage: getfh.exe [-h][-v][-n][-p] path \n");
+	COUTCS("   -v          Verbose mode\n");
+	COUTCS("   -h          Help\n");
+	COUTCSNR("   -w          WhatIf: no actions, get url, test connection.");
+	COUTY("  No downloads");
+	COUTCS("   -n          No banner\n");
+	COUTCS("   -p path     Destination path\n");
+	std::wcout << std::endl;
+}
+
+
+
+void writeFileBytes(const char* filename, const char* buffer, int size) {
+	COUTRS("Saving file %s.\n", filename);
+	std::ofstream outfile(filename, std::ios::out | std::ios::binary);
+	// write to outfile
+	outfile.write(buffer, size);
+
+	outfile.close();
+}
+
+string log(const Request& req, const Response& res) {
+	string s;
+	char buf[BUFSIZ];
+
+	s += "================================\n";
+
+	snprintf(buf, sizeof(buf), "%s %s %s", req.method.c_str(),
+		req.version.c_str(), req.path.c_str());
+	s += buf;
+
+	string query;
+	for (auto it = req.params.begin(); it != req.params.end(); ++it) {
+		const auto& x = *it;
+		snprintf(buf, sizeof(buf), "%c%s=%s",
+			(it == req.params.begin()) ? '?' : '&', x.first.c_str(),
+			x.second.c_str());
+		query += buf;
+	}
+	snprintf(buf, sizeof(buf), "%s\n", query.c_str());
+	s += buf;
+
+	s += dump_headers(req.headers);
+
+
+	s += "--------------------------------\n";
+
+	snprintf(buf, sizeof(buf), "%d\n", res.status);
+	s += buf;
+	s += dump_headers(res.headers);
+
+	return s;
+}
+
+char* getDlUrl(string filename, string api_url, bool optVerbose) {
+
+	COUTYS("Retrieving Download URL for \"%s\"", filename.c_str());
+	httplib::Params params;
+	params.emplace("projectUri", "IrfanView.html");
+	params.emplace("fileName", filename);
+	params.emplace("releaseId", "623457812413750bd71fef36");
+	params.emplace("projectId", "5b8d1f5659eee027c3d7883a");
+	params.emplace("source", "CF");
+
+	if (optVerbose) {
+		string strParams = dump_params(params);
+		COUTMS("=== Request Parameters ===");
+		COUTM("%s", strParams.c_str());
+	}
+	httplib::Client cli(api_url);
+	cli.set_connection_timeout(5,0);
+	cli.set_read_timeout(5,0);
+	cli.set_keep_alive(true);
+	auto res = cli.Post("/download", params);
+
+	if (res->status != 200) {
+		COUTRS("Error %d.\n", res->status);
+		return nullptr;
+	}
+
+	std::string body = res->body;
+
+	const char* chr = body.c_str();
+	int len = body.size();
+	char* download_url = parseJson(chr, len);
+
+	if (optVerbose) {
+		COUTYRL("Download URL: ");
+		COUTRS("\"%s\"", download_url);
+	}
+	return download_url;
+}
+
+bool downloadFile(string dwl_url, string fullUrl, string outFile, bool optVerbose) {
+
+	bool result = false;
+	vector<string> v = split(fullUrl, '/');
+	char* str = (char*)malloc(512);
+	strcpy(str, "/");
+	for (int i = 3; i < v.size(); i++) {
+		strcat(str, v[i].c_str());
+		if (i < (v.size() - 1)) { strcat(str, "/"); }
+	}
+	try {
+		httplib::Headers h;
+
+		h.emplace("sec-ch-ua", "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"99\", \"Google Chrome\";v=\"99\"");
+		h.emplace("sec-ch-ua-mobile", "?0");
+		h.emplace("sec-ch-ua-platform", "Windows");
+		h.emplace("Upgrade-Insecure-Requests", "1");
+		h.emplace("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36");
+		h.emplace("Sec-Fetch-Site", "same-site");
+		h.emplace("Sec-Fetch-Mode", "navigate");
+		h.emplace("Sec-Fetch-Dest", "document");
+		h.emplace("Referer", "https=//www.fosshub.com/");
+		h.emplace("Accept-Encoding", "gzip, deflate, br");
+
+		COUTYS("Requesting File to server %s.\nRequest: %s", dwl_url.c_str(), str);
+
+		if (optVerbose) {
+			string strHeaders = dump_headers(h);
+			COUTMS("=== Request Headers ===");
+			COUTM("%s", strHeaders.c_str());
+		}
+
+		string rcvdata;
+
+		httplib::Client cli(dwl_url);
+		if (optVerbose) {
+			cli.set_logger(
+				[](const Request& req, const Response& res) {
+					string l = log(req, res);
+					COUTM("%s", l.c_str());
+				});
+		}
+		cli.set_connection_timeout(5,0);
+		cli.set_read_timeout(5,0);
+		cli.set_keep_alive(true);
+		auto getres = cli.Get(str, h,
+			[&](const char* data, size_t data_length) {
+				rcvdata.append(data, data_length);
+				if (optVerbose) {
+					COUTRS("Received %d bytes. Total: %d", data_length, rcvdata.size());
+				}
+				return true;
+			}
+			//,[&](uint64_t offset, uint64_t total_length) { COUTMS("received %d/%s", offset, total_length); return true; }
+		);
+		if (getres->status == 200) {
+			writeFileBytes(outFile.c_str(), rcvdata.data(), rcvdata.length());
+			result = true;
+		}
+
+	}
+	catch (...) { COUTRS("Caught Error.\n"); result = false; }
+	return result;
+}
+
+void testLogFunctions(const char * msg){
+	COUTTITLE("TITLE MESSAGE\n");
+
+
+	COUTINFO("VERY IMPORTANT INFORMATION\n");
+	
+}
+
+
+
+
+void logError(const char * msg){
+	COUTMM("[ERROR] ");
+	COUTY("%s", msg);
+}
+
+
+// Sends a raw request to a server listening at HOST:PORT.
+bool testConnection(const char * host, int port, time_t conn_timeout_sec, time_t read_timeout_sec, time_t write_timeout_sec) {
+  auto error = Error::Success;
+
+  auto client_sock = detail::create_client_socket(
+      host, "", port, AF_UNSPEC, false, nullptr,
+      conn_timeout_sec, 0,
+      read_timeout_sec, 0,
+      write_timeout_sec, 0, std::string(), error);
+
+  if (client_sock == INVALID_SOCKET) { return false; }
+
+  detail::close_socket(client_sock);
+
+  return true;
+}
